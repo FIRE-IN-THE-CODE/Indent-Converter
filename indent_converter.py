60b6802a621d51fe707a4fe580d0ede21b3515cd
Mr.K <magical.coding@protonmail.com>
2020-07-29 21:32:38 -0400

    made improvements

--- indent_converter.py
+++ indent_converter.py
@@ -7,82 +7,77 @@ def usage():
     print("Indent Converter: a small script used to reformat other python scripts"
           + " converting tabs into 4 spaces or vice versa.")
     print("\n")
-    print("Note: it is recommended to backup any important scripts before "
-          + " using this program in case anything unexpected happens.")
-    print("Options: ")
-    print('Syntax: [-f (name of the file)] [-m or -r] [-o if desired]')
-    print("-f	--file		select the script to convert")
+    print("Syntax format: ")
+    print('<command> [file name] [options]')
+    print('\n')
+    print('Commands:')
     print("-h	--help		prints usage information")
     print('-m   --mix       mixes the indents into tabs and spaces')
-    print("-o	--output	gives the name to the new converted file")
     print('-r   --repair    fixes the script to make all indents into 4 spaces.')
-
+    print('Options:')
+    print('-o	--output	gives a new name to the new converted file')
+    print('\n')
     print('Author: www.github.com/FIRE-IN-THE-CODE')
 
-''' OUT FOR DEBUGGING
-def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'f:ho:', ['file=', 'help', 'output='])
 
-    for o, a in opts:
-        if o in ('-f', '--file'):
-            target_script = a
-        elif o in ('-h', '--help'):
-            usage()
-        elif o in ('-o', '--output'):
-            result_file = a
-'''
-
-
-def mixer():
-    code_lines = []
+def mixer(file_target, new_file_name):
+    original_code = []
     new_code = []
 
-    file_target = open('test.py', 'r')     # Temporarily has the value 'test1.py' for debugging
-    for line in file_target.readlines():
-        code_lines.append(line)
-    file_target.close()
+    open_target_file = open(file_target, 'r')
+    for code_line in open_target_file.readlines():
+        original_code.append(code_line)
+    open_target_file.close()
 
-    for item in code_lines:
-        original_code_line = []
-        new_code_line = ''
-        for letter in item:
-            original_code_line.append(letter)
-        print('Value of tmp_list: ', original_code_line)  # Debugging only
-        try:
-            chance = random.randrange(1, 101)
-            if original_code_line[0] == ' ' and original_code_line[1] == ' ' \
-                    and original_code_line[2] == ' ' and original_code_line[3] == ' ':
-                if 0 < chance < 50:
-                    original_code_line[0] = ' '
-                elif 50 < chance < 101:
-                    original_code_line[0] = '\t'
-                print('The value of tmp_list 1: ', original_code_line)      # Debugging
-                for i in original_code_line: new_code_line = new_code_line + i
-                new_code.append(new_code_line)
-                del original_code_line, new_code_line
-            elif original_code_line[0] == '\t':
-                if 0 < chance < 50:
-                    original_code_line[0] = '    '
-                elif 50 < chance < 101:
-                    original_code_line[0] = '\t'
-                print('The value of tmp_list 2: ', original_code_line)        # Debugging
-                for i in original_code_line: new_code_line += i
-                new_code.append(new_code_line)
-                del original_code_line, new_code_line
+    for line in original_code:
+        processing_code = ''
+        indent = ''
+        code_to_add = indent + processing_code
+        tab_count = 0
+        space_count = 0
+        space_counter = 0
+
+        # Remove spaces and tabs and count the number of indents
+        for byte in line:
+            if byte == ' ':
+                space_counter += 1
+                del byte
+            elif byte == '\t':
+                tab_count += 1
+                del byte[0]
             else:
-                new_code.append(item)
-        except IndexError:
-            pass
+                pass
+            try:
+                processing_code += byte
+            except NameError:                           # May be NameError or localVariable referenced something
+                pass
+        space_count += space_counter/4
 
-        print('Value of the new_code list: ', new_code)         # Debugging
+        # Modify the indents and add the new code line
+        while space_count != 0:
+            chance = random.randrange(1, 101)
+            if 0 < chance < 50:
+                indent += ' '
+            elif 50 < chance < 102:
+                indent += '\t'
+            space_count -= 1
+        new_code.append(code_to_add)
 
-    result_file = open('test.py', 'w')                          # 'test.py' for debugging
-    for line in new_code:
-        result_file.write(line)
-    result_file.close()
+    # Create the file and add the code
+    print('Value of the new_code list: ', new_code)         # Debugging
+    if new_file_name == '':
+        result_file = open(file_target + '_new.py', 'w')
+        for line in new_code:
+            result_file.write(line)
+        result_file.close()
+    else:
+        result_file = open(new_file_name, 'w')
+        for line in new_code:
+            result_file.write(line)
+        result_file.close()
 
 
-def fixer():
+def fixer(file_name, new_file_name):
     original_code_lines = []
     new_code_lines = []
 
@@ -100,5 +95,23 @@ def fixer():
                 new_code_lines += line
 
 
+def main():
+    target_file = 'test.py'         # Debugging
+    result_file = ''
+    opts, args = getopt.getopt(sys.argv[1:], 'hmo:', ['help', 'mix', 'repair=', 'output='])  # Took out output settings
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+        elif o in ('-o', '--output'):
+            result_file = a
+        elif o in ('-m', '--mix'):
+            print('Result_file value: ', result_file)       # Debugging
+            mixer(target_file, result_file)
+        elif o in ('-r', '--repair'):
+            target_file = a
+            fixer(target_file, result_file)
+
+
 if __name__ == '__main__':
-    mixer()
+    main()
